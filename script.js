7fa31898021065dca0b515dabffb472d44bca1f4
Glitch (personality-quiz-starter-code) <none>
2020-07-09 04:18:28 +0000

    ☂️🐕 Checkpoint ./.glitch-assets:18544173/446 ./index.html:18544173/899 ./script.js:18544173/51

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 //This is where you will define the variables you will be using in your project.
 //#TODO: Create four variables to track each possible quiz outcome
 var questionCount = 0;
-var result1Score = 0;
-var result2Score = 0;
-var result3Score = 0;
-var result4Score = 0;
+var fireScore = 0;
+var earthScore = 0;
+var waterScore = 0;
+var airScore = 0;
 
 
 
