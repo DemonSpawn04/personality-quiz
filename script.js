23336bc5fad898237b337729d322dd0335002113
Glitch (personality-quiz-starter-code) <none>
2020-07-10 03:56:11 +0000

    👸🏮 Checkpoint ./script.js:18544173/469

--- script.js
+++ script.js
@@ -13,7 +13,17 @@ var airScore = 0;
 
 //#TODO: Use the DOM to create variables for the first quiz question.
 var q1a1 = document.getElementById("q1a1");
-var q1a2 = document.getElementById("q")
+var q1a2 = document.getElementById("q1a2");
+var q1a3 = document.getElementById("q1a3");
+var q1a4 = document.getElemnetById("q1a4");
+var q2a1 = document.getElementById("q2a1");
+var q2a2 = document.getElementById("q2a2");
+var q2a3 = document.getElementById("q2a3");
+var q2a4 = document.getElementById("q2a4");
+var q3a1 = document.getElementById('q3a1');
+var q3a2 = document.getElementById('q3a2');
+var q3a3 = document.getElementById('q3a3');
+var q3a4 = document.getElementById('q3a4');
 
 
 
@@ -24,7 +34,7 @@ q1a1.addEventListener("click", fire);
 q1a1.addEventListener("click", earth);
 q1a1.addEventListener("click", water);
 q1a1.addEventListener("click", air);
-
+q
 
 
 
